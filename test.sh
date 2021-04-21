fcadb8b46d15c132a1655357b92e8b8f5c8dcecf
Schlöffel, Joachim <schloeffel@financial-service-plus.de>
2021-04-21 09:04:10 +0200

    fix tests

--- test.sh
+++ test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-set -ex
+set -xe
 docker-compose up -d --build --force-recreate --remove-orphans test-server
-curl http://127.0.0.1:8080/folder/test.txt > test.txt
-cmp test.txt test/htdocs/folder/test.txt
-rm test.txt
+curl http://127.0.0.1:8080/folder/ > index.html
+docker-compose down
+cmp index.html test/htdocs/folder/index.html
+rm index.html
